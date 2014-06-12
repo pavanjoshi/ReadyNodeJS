ec20466240310e9f3f5c1f537bc6b852138a03fc
pavanjoshi <pjpavan2@gmail.com>
2014-06-12 17:43:44 +0530

    Create nodehttpget.js
    Needs 'http' basic node package only.

---
+++ nodehttpget.js
@@ -0,0 +1,22 @@
+// JSON GET CALL WITH PROXY
+var url = 'your url to get json response';
+var options = {
+  host: "proxy",
+  port: 8080,
+  path: url
+};
+
+http.get(options, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        var jsonResponse = JSON.parse(body)
+        console.log("Got response: ", jsonResponse.get24HoursVideos[0].name);
+    });
+}).on('error', function(e) {
+      console.log("Got error: ", e);
+});
